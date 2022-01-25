8377fdf47e6e18337d63ea5ae3ccbc59c22ca0dd
cyandev <djs.bitcoin@gmail.com>
2022-01-25 16:10:16 +0000

    pipeline

--- src/analysis/analysisPipeline.js
+++ src/analysis/analysisPipeline.js
@@ -15,14 +15,14 @@ for (let executableFile of fs.readdirSync(__dirname + "/transformers")) {
     let transformer = require("./transformers/" + executableFile);
 
     if ("tmp" in transformer) { //the transformer supports teamMatchPerformances
-        console.log(`${transformer.tmp.name}.tmp - tmp transformer detected`)
+        console.log(`${transformer.tmp.name}.tmp - detected`)
         if (transformer.tmp.name in transformers.tmp) 
             throw new Error(`${transformers.tmp.name}.tmp - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(transformers.tmp)}]`);
         transformers.tmp[transformer.tmp.name] = transformer.tmp;
     }
 
     if ("team" in transformer) { //the transformer supports teams
-        console.log(`${transformer.team.name}.team - team transformer detected`)
+        console.log(`${transformer.team.name}.team - detected`)
         if (transformer.team.name in transformers.team) 
             throw new Error(`${transformers.team.name} - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(transformers.tmp)}]`);
         transformers.team[transformer.team.name] = transformer.team;
@@ -32,7 +32,7 @@ console.log("loaded all transformers!");
 console.log(transformers);
 
 
-async function execute() {
+async function execute(dataset) {
     /* get tmps from database */
     let dataset = new Dataset((await TeamMatchPerformance.find()).map((o) => o.toObject()));
     
@@ -40,7 +40,8 @@ async function execute() {
         console.log(`running ${tfConfig.name}...`)
         dataset = transformers[tfConfig.type][tfConfig.name].execute(dataset, tfConfig.outputPath, tfConfig.options);
     }
-    
+    console.log("complete!")
+
     return dataset
 }
 
