240778e24c60eae6203359d92ef7778f90a31986
cyandev <djs.bitcoin@gmail.com>
2022-02-02 22:52:36 -0600

    add eventNumber to match-scouting

--- src/analysis/analysisPipeline.js
+++ src/analysis/analysisPipeline.js
@@ -2,7 +2,10 @@ const pipelineConfig = require("../../config/analysis-pipeline.json");
 const fs = require("fs");
 const { TeamMatchPerformance } = require("../lib/db");
 const { Dataset } = require("./DataTransformer");
+const chalk = require("chalk");
 
+//enable debug logs
+const debug = false
 
 /* load transformers */
 const transformers = {
@@ -11,25 +14,24 @@ const transformers = {
 }
 
 for (let executableFile of fs.readdirSync(__dirname + "/transformers")) {
-    console.log(`attempting to load transformer from ${__dirname + "/transformers/" + executableFile}`)
+    if (debug) console.log(`attempting to load transformer from ${__dirname + "/transformers/" + executableFile}`)
     let transformer = require("./transformers/" + executableFile);
 
     if ("tmp" in transformer) { //the transformer supports teamMatchPerformances
-        console.log(`${transformer.tmp.name}.tmp - detected`)
-        if (transformer.tmp.name in transformers.tmp) 
-            throw new Error(`${transformers.tmp.name}.tmp - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(transformers.tmp)}]`);
+        if (debug)console.log(`${transformer.tmp.name}.tmp - detected`)
+        if (transformer.tmp.name in transformers.tmp)
+            throw new Error(chalk.whiteBright.bgRed.bold(`${transformers.tmp.name}.tmp - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(transformers.tmp)}]`));
         transformers.tmp[transformer.tmp.name] = transformer.tmp;
     }
 
     if ("team" in transformer) { //the transformer supports teams
-        console.log(`${transformer.team.name}.team - detected`)
+        if (debug) console.log(`${transformer.team.name}.team - detected`)
         if (transformer.team.name in transformers.team) 
             throw new Error(`${transformers.team.name} - transformer name duplicated! Transformer names must be unique.\nOther transformers loaded: [${Object.keys(transformers.tmp)}]`);
         transformers.team[transformer.team.name] = transformer.team;
     }
 }
-console.log("loaded all transformers!");
-console.log(transformers);
+if (debug) console.log("loaded all transformers!");
 
 
 async function execute(dataset,debug=false) {
@@ -37,10 +39,10 @@ async function execute(dataset,debug=false) {
     dataset = new Dataset((await TeamMatchPerformance.find()).map((o) => o.toObject()));
     
     for (let tfConfig of pipelineConfig) {
-        console.log(`running ${tfConfig.name} - ${JSON.stringify(tfConfig.options)}`)
+        if (debug) console.log(`running ${tfConfig.name} - ${JSON.stringify(tfConfig.options)}`)
         dataset = transformers[tfConfig.type][tfConfig.name].execute(dataset, tfConfig.outputPath, tfConfig.options);
     }
-    console.log("complete!")
+    if (debug) console.log("complete!")
 
     return dataset
 }
