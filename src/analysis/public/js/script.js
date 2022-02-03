bcf94553f5259b32a18c21991179560de41f38c8
nithilan4 <43860191+nithilan4@users.noreply.github.com>
2022-02-03 15:10:01 -0600

    document script.js

--- src/analysis/public/js/script.js
+++ src/analysis/public/js/script.js
@@ -1,7 +1,10 @@
 (async () => {
+    //api keys and config
     const EVENT_ID = "2020ilch"
     const TBA_API_KEY = "35YGkhzv98FQInv2qLCZ6C9sixyhgU4hawV0oOS3jY0JikXVgAW2fKsAIKgZP8zx";
     //TODO: put api key into a server config and request
+
+    //modules object strucutre
     const modules = {
         team: [],
         match: {
@@ -9,8 +12,9 @@
             right: []
         }
     }
-    let dataset
 
+    //start loading animation, fetch modules config, fetch dataset, then initialize UI elements
+    let dataset
     await loadAround(async () => {
         const modulesConfig = await fetch(`/analysis/analysis-modules.json`).then(res => res.json());
         dataset = await fetchDataset()
@@ -34,18 +38,22 @@
 
     //team UI functions
     async function loadTeams(dataset, modulesConfig) {
+        //get blue alliance teams
         const allTeams = await fetchTeams()
+        //add to sidebar team list
         for (const [teamNumber, team] of Object.entries(dataset.teams)) {
             const teamContainer = constructTeam(teamNumber, team, allTeams)
             teamList.appendChild(teamContainer)
         }
 
+        //enable sidebar if sidebar modules exist
         if (modulesConfig.map(m => m.position).includes("side")) {
             teamView.classList.add("side-enabled")
         } else {
             teamView.classList.remove("side-enabled")
         }
 
+        //get all team modules, create and store module classes, then append their placeholder containers to lists
         for (const module of modulesConfig.filter(m => m.view == "team")) {
             const moduleObject = new moduleClasses[module.module](module)
             if (module.position == "main") {
@@ -58,6 +66,7 @@
     }
 
     function constructTeam(teamNumber, team, allTeams) {
+        //create and populate sidebar element
         const teamContainer = createDOMElement("div", "team-container")
         const teamNumDisplay = createDOMElement("div", "team-number")
         teamNumDisplay.innerText = teamNumber
@@ -68,6 +77,7 @@
             teamContainer.appendChild(teamNameDisplay)
         }
 
+        //switch to team on click of sidebar team, set module data
         teamContainer.addEventListener("click", async () => {
             await setTeamModules(teamNumber)
             displayTeam(teamContainer)
@@ -76,12 +86,14 @@
         return teamContainer
     }
 
+    //reset UI and switch to team view
     function displayTeam(teamContainer) {
-        resetAllSelected()
+        clearInterface()
         teamContainer.classList.add("selected")
         showFade(teamView)
     }
 
+    //call setData on every module in teams
     async function setTeamModules(teamNumber) {
         for (const module of modules.team) {
             await module.setData(await module.formatData([teamNumber], dataset))
@@ -90,13 +102,17 @@
 
     //match UI functions
     async function loadMatchView(dataset, modulesConfig) {
+        //add event listener to "Simulate Match" button to set reset UI and switch to match view
         matchViewSwitch.addEventListener("click", () => {
-            resetAllSelected()
+            clearInterface()
             matchViewSwitch.classList.add("selected")
             showFade(matchView)
         })
 
+        //get all dropdowns
         const teamSelects = Array.from(document.querySelectorAll(".alliance-selects")).map(g => Array.from(g.children)).flat()
+        
+        //populate dropdowns with team numbers
         for (const teamSelect of teamSelects) {
             for (const team of Object.keys(dataset.teams)) {
                 const option = createDOMElement("option")
@@ -105,15 +121,16 @@
                 teamSelect.appendChild(option)
             }
 
+            //on dropdown change
             teamSelect.addEventListener("change", async () => {
+                //apply dropdown border style if selected
                 if (teamSelect.value === "none") {
                     teamSelect.classList.remove("filled")
                 } else {
                     teamSelect.classList.add("filled")
                 }
 
-                console.log(teamSelects.map(s => s.value))
-
+                //hide alliance module lists if no teams are selected for that alliance
                 if (teamSelects.map(s => s.value).slice(0, 3).every(v => v === "none")) {
                     leftAllianceModules.classList.add("hidden")
                 } else {
@@ -126,10 +143,12 @@
                     rightAllianceModules.classList.remove("hidden")
                 }
 
+                //set data on match modules
                 await setMatchModules([teamSelects.slice(0, 3).map(s => s.value).filter(s => s !== "none"), teamSelects.slice(0, 3).map(s => s.value).filter(s => s !== "none")])
             })
         }
 
+        //create match module objects and append placeholders to module list elements
         for (const module of modulesConfig.filter(m => m.view == "match")) {
             const leftModuleObject = new moduleClasses[module.module](module)
             leftAllianceModules.appendChild(leftModuleObject.container)
@@ -141,9 +160,8 @@
         }
     }
 
+    //call setData on every module in matches
     async function setMatchModules(alliances) {
-        console.log(alliances, modules)
-
         for (const module of modules.match.left) {
             await module.setData(await module.formatData(alliances[0], dataset))
         }
@@ -153,14 +171,14 @@
         }
     }
 
-    //dashboard initializer (loads teams and match view)
+    //dashboard initializer, loads teams and match view
     function initDashboard(dataset, modulesConfig) {
         loadTeams(dataset, modulesConfig)
         loadMatchView(dataset, modulesConfig)
     }
 
-    //util
-    function resetAllSelected() {
+    //reset the UI to state of nothing shown, nothing selected
+    function clearInterface() {
         Array.from(document.querySelector("#team-list").children).map(t => t.classList.remove("selected"))
         hideFade(welcomeView)
         hideFade(matchView)
