f997f6ae5be561dc7f1eba1824808e209404876f
nithilan4 <43860191+nithilan4@users.noreply.github.com>
2022-03-17 18:21:46 -0500

    disable on offline submission w/ reload timeout

--- src/scouting/public/js/match-scouting.js
+++ src/scouting/public/js/match-scouting.js
@@ -90,9 +90,18 @@ let devEnd
             button.element.addEventListener("click", async () => {
                 // Handle click after timer runs out
                 if (time <= 0) {
+					for (const button of buttons) {
+						button.element.classList.add("disabled")
+					}
                     new Popup("notice","Submitting Data...",1000);
                     await LocalData.storeTeamMatchPerformance(new TeamMatchPerformance(actionQueue).data);
-                    await ScoutingSync.sync();
+                    setTimeout(() => {
+						new Popup("error","Couldn't submit, syncing later",1000);
+						setTimeout(() => {
+							window.location.reload()
+						}, 1000)
+					}, 5000)
+					await ScoutingSync.sync();
                     await ScoutingSync.updateState({status: ScoutingSync.SCOUTER_STATUS.COMPLETE});
                     window.location.reload();
                 }
