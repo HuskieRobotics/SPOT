7eab508e7030b92d916560fb88d8369523e063fc
nithilan4 <43860191+nithilan4@users.noreply.github.com>
2022-03-18 20:50:07 -0500

    ranks log

--- src/analysis/modules/Stats/index.js
+++ src/analysis/modules/Stats/index.js
@@ -46,7 +46,10 @@ class Stats {
 					})
 
 					totalRanked = filteredTeams.length
-					statRank = filteredTeams.sort((a, b) => (this.applyModifiers(stat, getPath(dataset.teams[b], stat.path, 0)) - this.applyModifiers(stat, getPath(dataset.teams[a], stat.path, 0))) * stat.sort).indexOf(teams[0]) + 1
+					const rankedTeams = filteredTeams.sort((a, b) => (this.applyModifiers(stat, getPath(dataset.teams[b], stat.path, 0)) - this.applyModifiers(stat, getPath(dataset.teams[a], stat.path, 0))) * stat.sort)
+					statRank = rankedTeams.indexOf(teams[0]) + 1
+					console.log(stat.name)
+					console.log(rankedTeams.map(t => `${t}: ${this.applyModifiers(stat, getPath(dataset.teams[t], stat.path, 0))}`))
 				}
 
 				if (stat.decimals !== undefined) {
