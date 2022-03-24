32e45998afbd3f3745bb659981d4cddf79bb9885
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-03-23 22:03:04 -0400

    Update match-scouting.js

--- src/scouting/public/js/match-scouting.js
+++ src/scouting/public/js/match-scouting.js
@@ -65,7 +65,7 @@ let devEnd
         },
 
         "match-control": (button) => {
-			ScoutingSync.sync();
+			await ScoutingSync.sync();
             button.element.innerText = "Start Match" + " | Your Team: " + ScoutingSync.state.robotNumber;
             button.element.addEventListener("click", async () => {
                 // Handle click after timer runs out
