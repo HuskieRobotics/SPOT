3eb8ce08b322ccf64389cde53217fa785554ba74
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-05 12:29:16 -0400

    fix?

--- src/scouting/public/js/instructions.js
+++ src/scouting/public/js/instructions.js
@@ -1,8 +1,10 @@
 let page = "landing"
 document.querySelector("#instructions .ok").addEventListener("click", async () => {
+    console.log("switched to page "+ page)
     switchPage(page)
 })
 
-function setPage(page){
-    this.page = page
+function setPage(newPage){
+    page = newPage
+    console.log("set to page "+ page)
 }
