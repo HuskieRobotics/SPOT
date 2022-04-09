d9675c3fd1f6e6e120318fe33407b0efdbed5b3b
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-04-08 20:29:44 -0400

    maybe fix

--- src/scouting/public/js/scouting-sync.js
+++ src/scouting/public/js/scouting-sync.js
@@ -106,7 +106,14 @@ class ScoutingSync {
     static updateState(stateUpdate, incoming = false) {
         return new Promise((res, rej) => {
             Object.assign(ScoutingSync.state, stateUpdate);
-			const updateMatch = ScoutingSync.matches.find(m => m.number === ScoutingSync.state.matchNumber)
+            let data = ScoutingSync.matches.find(m => m.number === ScoutingSync.state.matchNumber)
+            let updateMatch = "";
+            if (data !== "undefined"){
+			    updateMatch = data;
+            }
+            else{
+                updateMatch = false;
+            }
 			if (updateMatch) {
 				document.querySelector(".scouting-info").innerText = `Match: ${ScoutingSync.state.matchNumber} | Team: ${ScoutingSync.state.robotNumber}`
 				document.querySelector(".scouting-info").style.color = updateMatch.robots.red.includes(ScoutingSync.state.robotNumber) ? "var(--error)" : "var(--accent)"
