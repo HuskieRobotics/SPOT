8cc0d31d01077157d25576884d7a2792dd37fcaf
Aidan Donnelly <73967906+AidanDonnelly1@users.noreply.github.com>
2023-01-19 11:35:27 -0600

    finished compareAlliances; added documentation

--- src/analysis/public/js/autoPick.js
+++ src/analysis/public/js/autoPick.js
@@ -1,4 +1,4 @@
-const { zScore } = require("simple-statistics");
+const { zScore, cumulativeStdNormalProbability } = require("simple-statistics");
 const { Dataset } = require("../../DataTransformer");
 
 function swap(arr, i, j) {
@@ -71,41 +71,74 @@ async function fetchTeams() {
 		return acc
 	}, {})
 }
-
+/**
+ * 
+ * @param {A list of all teams} teams 
+ * @returns A list of all possible alliances, including impossible allaicnes(duplicate teams on an alliance)
+ */
 function possibleAliiances(teams) {
-	const possibleAlliiances = [] // an array of all possible alliances 
+	let possibleAlliiances = [] // an array of all possible alliances 
 	for(x in teams) {
 		for(y in teams) {
 			for(z in teams) {
-				possibleAliiances.push(new alliance[x, y, z]) //FIXME alliances of the same team exist 
+				possibleAliiances.push(new alliance[x, y, z])
 			}
 		}
 	}
+	return possibleAliiances
 }
-
+/**
+ * 
+ * @param {An alliance of three teams} alliance 
+ * @returns the average score for an alliance 
+ */
 function allianceAverage(alliance) {
 	return alliance[0].averageScore + alliance[1].averageScore + alliance[2].averageScore;
 }
 
+/**
+ * 
+ * @param {An alliance of three teams} alliance 
+ * @returns the standard deveiation of an alliances score 
+ */
 function allianceStandardDeviation(alliance) {
 	let i = Math.pow(alliance[0].standardDeviation, 2) + Math.pow(alliance[1].standardDeviation, 2) + Math.pow(alliance[2].standardDeviation, 2)
-	return staMath.sqrt(i);
+	return Math.sqrt(i);
 }
-
+/**
+ * 
+ * @param {An alliance of three teams} alliance1 
+ * @param {An alliance of three teams} alliance2 
+ * @returns the average difference in score between two alliances, a postive number means on average alliance 1 wins
+ */
 function matchAverage(alliance1, alliance2) {
 	return allianceAverage(alliance1) - allianceAverage(alliance2);
 }
 
+/**
+ * 
+ * @param {An alliance of three} alliance 
+ * @returns the the standard deveation in the difference of the score between two alliances
+ */
 function matchStandardDeviation(alliance1, alliance2) {
 	let i = Math.pow(allianceStandardDeviation(alliance), 2) + Math.pow(allianceStandardDeviation(alliance), 2);
 	return Math.sqrt(i);
 }
+/**
+ * 
+ * @param {*} alliance1 
+ * @param {*} alliance2 
+ * @returns the probiblity that alliance 1 wins a random match between the two alliances 
+ */
 
-function compareAlliances {
-	zscore = zScore()
+function compareAlliances(alliance1, alliance2) {
+	zscore = zScore(0, matchAverage(alliance1, alliance2), matchStandardDeviation(alliance1, alliance2))
+	probAlliance2Wins = cumulativeStdNormalProbability(zscore)
+	return 1 - probAlliance2Wins;
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------//
+
 const dataset = fetchDataset();
 const teams = fetchTeams();
 const possibleAliiances = possibleAliiances(teams);
