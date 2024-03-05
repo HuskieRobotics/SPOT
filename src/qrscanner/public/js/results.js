5bd9ae986aac5e9ffcf0a74ce741aae98ac8261d
Nathan Pease <nspease@stu.naperville203.org>
2024-03-05 11:00:29 -0600

    QR Code is functional now, working on styling

--- src/qrscanner/public/js/results.js
+++ src/qrscanner/public/js/results.js
@@ -1,7 +1,12 @@
 async function onScanSuccess(qrCodeMessage) {
     const data = await decodeQRCodeUrl(qrCodeMessage);
-    document.getElementById('result').innerHTML = '<p>' + data + '</p>';
-    // document.getElementById('result').innerHTML = '<p>' + qrCodeMessage + '</p>';
+    let html = `<p>Timestamp: ${data.timestamp}<br>Client Version: ${data.clientVersion}<br>Scouter ID: ${data.scouterId}`;
+    html += `<br>Event Number: ${data.eventNumber}<br>Match Number: ${data.matchNumber}<br>Robot Number: ${data.robotNumber}<br>Match ID: [<br></p>`;
+    for (const action of data.actionQueue) {
+        html += `{ id: '${action.id}', ts: ${action.ts} }<br>`
+    }
+    html += ']</p>';
+    document.getElementById('result').innerHTML = html;
 }
 
 function onScanError(errorMessage) {
