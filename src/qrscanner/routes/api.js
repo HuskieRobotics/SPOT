6d05d9d24c4ff9902f57dcbfb2249036d0cb028e
Nathan Pease <nspease@stu.naperville203.org>
2024-03-13 11:17:21 -0500

    Got QR code data to upload to database, had to modify action schema for qr.json to allow for more bits for TS since it was too small initially

--- src/qrscanner/routes/api.js
+++ src/qrscanner/routes/api.js
@@ -4,10 +4,27 @@ const { TeamMatchPerformance } = require('../../lib/db');
 let router = Router();
 
 router.post('/teamMatchPerformance', (req, res) => {
-    console.log(req.body);
-    console.log(TeamMatchPerformance);
-
-    // const teamMatchPerformance 
+    let teamMatchPerformance;
+    try {
+        teamMatchPerformance = new TeamMatchPerformance({
+            timestamp: req.body.timestamp,
+            clientVersion: req.body.clientVersion,
+            scouterId: req.body.scouterId,
+            robotNumber: req.body.robotNumber,
+            matchNumber: req.body.matchNumber,
+            eventNumber: req.body.eventNumber,
+            matchId_rand: req.body.matchId_rand,
+            actionQueue: req.body.actionQueue,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+    try {
+        teamMatchPerformance.save();
+    } catch (err) {
+        console.log(err);
+    }
 
     res.status(201).end();
 });
