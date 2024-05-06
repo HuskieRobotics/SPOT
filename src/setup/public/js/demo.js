1994011f5f160d65528a553b2aa760d97623cfd2
Allison Shi <azshi@stu.naperville203.org>
2024-05-06 10:22:12 -0500

    demo mode continued efforts...

--- src/setup/public/js/demo.js
+++ src/setup/public/js/demo.js
@@ -1,16 +1,12 @@
 // the toggle switch for demo
 document.addEventListener('DOMContentLoaded', function () {
     const toggleSwitch = document.getElementById('DEMO');
-
+    toggleSwitch.value = false;
     toggleSwitch.addEventListener('change', function () {
         if (this.checked) {
             toggleSwitch.value = true;
             console.log(toggleSwitch.value);
             console.log("Switch is ON");
-        } else {
-            toggleSwitch.value = false;
-            console.log(toggleSwitch.value);
-            console.log("Switch is OFF");
-        }
+        } 
     });
 });
