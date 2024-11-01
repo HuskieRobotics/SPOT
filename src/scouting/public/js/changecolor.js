b05e70cdf1f5891ad0ecb3acecf5e3c1b3509fad
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2024-11-01 13:47:52 -0500

    Moving farther to updating everything to have dark

--- src/scouting/public/js/changecolor.js
+++ src/scouting/public/js/changecolor.js
@@ -1,12 +1,21 @@
 function changesheet() {
   let theme = document.getElementById("pagestyle");
   let status = document.getElementById("connection-status");
+  let form = document.getElementById("landing-style");
+  let scout = document.getElementById("match-color");
+  let wait = document.getElementById("wait-color");
 
-  if (theme.getAttribute("href") == "/css/landing2.css") {
-    theme.setAttribute("href", "/css/landing.css");
+  if (theme.getAttribute("href") == "/css/landing.css") {
+    theme.setAttribute("href", "/css/landing-dark.css");
     status.style.color = "#ac56d1";
+    form.setAttribute("href", "css/form-dark.css");
+    scout.setAttribute("href", "css/match-scouting-dark.css");
+    wait.setAttribute("href", "css/waiting-dark.css");
   } else {
-    theme.setAttribute("href", "/css/landing2.css");
+    theme.setAttribute("href", "/css/landing.css");
     status.style.color = "#4caf50";
+    form.setAttribute("href", "css/form.css");
+    scout.setAttribute("href", "css/match-scouting.css");
+    wait.setAttribute("href", "css/waiting.css");
   }
 }
