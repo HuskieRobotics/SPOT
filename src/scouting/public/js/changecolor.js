e0c8e2df6b1a7517df8aa41e7242d914912826d3
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2024-11-01 15:02:38 -0500

    Finished Dark Mode

--- src/scouting/public/js/changecolor.js
+++ src/scouting/public/js/changecolor.js
@@ -4,6 +4,7 @@ function changesheet() {
   let form = document.getElementById("landing-style");
   let scout = document.getElementById("match-color");
   let wait = document.getElementById("wait-color");
+  let vstr = document.getElementById("v-string");
 
   if (theme.getAttribute("href") == "/css/landing.css") {
     theme.setAttribute("href", "/css/landing-dark.css");
@@ -11,6 +12,7 @@ function changesheet() {
     form.setAttribute("href", "css/form-dark.css");
     scout.setAttribute("href", "css/match-scouting-dark.css");
     wait.setAttribute("href", "css/waiting-dark.css");
+    vstr.style.background = "#4f0d6b";
   } else {
     theme.setAttribute("href", "/css/landing.css");
     status.style.color = "#4caf50";
