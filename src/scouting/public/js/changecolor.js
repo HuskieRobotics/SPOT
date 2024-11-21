5bfb33704ca3c1334dd02e9f68e4cba9b3ae572a
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2024-11-20 18:57:06 -0600

    ALMOST DONE

--- src/scouting/public/js/changecolor.js
+++ src/scouting/public/js/changecolor.js
@@ -1,30 +1,63 @@
-function changesheet() {
-  let theme = document.getElementById("pagestyle");
+function changesheet() {}
+
+function goto() {
+  window.open("https://scouting.team3061.org/analysis/").focus;
+}
+
+function toggleSettingsMenu() {
+  var overlay = document.getElementById("settingsOverlay");
+  if (overlay.style.display === "none" || overlay.style.display === "") {
+    overlay.style.display = "flex";
+  } else {
+    overlay.style.display = "none";
+  }
+}
+
+window.onclick = function (event) {
+  var overlay = document.getElementById("settingsOverlay");
+  if (event.target === overlay) {
+    overlay.style.display = "none";
+  }
+};
+
+function toggleDarkMode() {
+  var body = document.body;
+  let vstr = document.getElementById("v-string");
   let status = document.getElementById("connection-status");
   let form = document.getElementById("landing-style");
   let scout = document.getElementById("match-color");
   let wait = document.getElementById("wait-color");
-  let vstr = document.getElementById("v-string");
 
-  if (theme.getAttribute("href") == "/css/landing.css") {
-    theme.setAttribute("href", "/css/landing-dark.css");
+  var statusElements = document.querySelectorAll(".status");
+  var menuImage = document.querySelector("#landing .menu-out .menu");
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    menuImage.src = "/img/menu-button-dark.png";
+    document
+      .getElementById("pagestyle")
+      .setAttribute("href", "/css/landing-dark.css");
+    statusElements.forEach(function (element) {
+      element.style.backgroundColor = "var(--dark-black)";
+    });
+    vstr.style.background = "#191b1c";
+    vstr.style.color = "#efefef";
     status.style.color = "#ac56d1";
     form.setAttribute("href", "css/form-dark.css");
     scout.setAttribute("href", "css/match-scouting-dark.css");
     wait.setAttribute("href", "css/waiting-dark.css");
-    vstr.style.background = "#191b1c";
-    vstr.style.color = "#efefef";
   } else {
-    theme.setAttribute("href", "/css/landing.css");
+    menuImage.src = "/img/menu-button.png";
+    document
+      .getElementById("pagestyle")
+      .setAttribute("href", "/css/landing.css");
+    vstr.style.background = "#efefef";
+    vstr.style.color = "#191b1c";
     status.style.color = "#4caf50";
     form.setAttribute("href", "css/form.css");
     scout.setAttribute("href", "css/match-scouting.css");
     wait.setAttribute("href", "css/waiting.css");
-    vstr.style.background = "#efefef";
-    vstr.style.color = "#191b1c";
+        statusElements.forEach(function(element)
+    element.style.backgroundColor = "var(--bg)";
   }
-}
-
-function goto() {
-  window.open("https://scouting.team3061.org/analysis/").focus;
 }
