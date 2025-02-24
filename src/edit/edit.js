da56b29c940e35b6cedca1bbeaaa9620d50da109
andrewsully20 <avsullivan@stu.naperville203.org>
2025-02-24 09:34:37 -0600

    Add authorization check for admin panel access - Not complete yet about halfway done

--- src/edit/edit.js
+++ src/edit/edit.js
@@ -1,11 +1,17 @@
 express = require("express");
+const config = require("../../config/config.json");
 
 let router = express.Router();
 
 router.use(express.static(__dirname + "/public"));
 
 router.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs");
+  if (req.headers.authorization === config.secrets.ACCESS_CODE) {
+    res.render(__dirname + "/views/index.ejs");
+  } else {
+    console.log("Unauthorized access attempt to admin panel");
+    res.redirect("/admin");
+  }
 });
 
 module.exports = router;
