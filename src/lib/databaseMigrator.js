223dcfc45ea3c3c3f12fab23567ac06a9f262a47
Geoff Schmit <gcschmit@gmail.com>
2025-09-14 09:29:43 -0500

    Migrate database from v4 to v5.

--- src/lib/databaseMigrator.js
+++ src/lib/databaseMigrator.js
@@ -8,16 +8,35 @@ mongoose.connect(config.secrets.DATABASE_URL, {
 });
 
 async function changeTypeOfEventCodes() {
-  // '1' -> 2025mndu2_official, '2' -> 2025ilch_official, '21' -> 2025ilch_testing
+  // // '1' -> 2025mndu2_official, '14' -> 2024ksla_training, '15' -> 2024ilch_training,
+  // '16' -> 2024joh_training, '19' -> 2024witw_training, '2' -> 2025ilch_official,
+  // '21' -> 2025ilch_testing, '8' -> 2023rr_testing, '98' -> 2025week0_training,
+  // '99' -> 2025week0_training, '3' -> 2025cur_official, '4' -> 2023ilch_training
+  // '7' -> 2023arc_training, '18' -> 2024rr_testing, '85' -> 2025mndu2_testing,
 
   // FIXME: update the eventNumbers array with the event numbers in your database
-  const eventNumbers = [1, 2, 21];
+  const eventNumbers = [
+    1, 2, 3, 4, 5, 7, 8, 14, 15, 16, 18, 19, 21, 85, 98, 99,
+  ];
 
   // FIXME: update the eventCodes array with the corresponding event codes
   const eventCodes = [
     "2025mndu2_official",
     "2025ilch_official",
+    "2025cur_official",
+    "2023ilch_training",
+    "2023mose_training",
+    "2023arc_training",
+    "2023rr_testing",
+    "2024ksla_training",
+    "2024ilch_training",
+    "2024joh_training",
+    "2024rr_testing",
+    "2024witw_training",
     "2025ilch_testing",
+    "2025mndu2_testing",
+    "2025week0_training",
+    "2025week0_training",
   ];
 
   try {
@@ -73,10 +92,10 @@ async function displayAllEventNumbers() {
 
 // Run the update once the connection is established
 mongoose.connection.once("open", () => {
-  displayAllEventNumbers();
+  // displayAllEventNumbers();
 
   // comment the previous line and uncomment the next line after updating the changeTypeOfEventCodes
   // to populate the mapping from event numbers to event codes
 
-  // changeTypeOfEventCodes();
+  changeTypeOfEventCodes();
 });
