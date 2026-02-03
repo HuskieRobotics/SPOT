ad4f9b1add6e9e5b695afd8a6d1054f06fc24127
KSM090108 <ksmcc2018@gmail.com>
2026-02-03 09:20:10 -0600

    add additional formatting and visual improvements to the radar chart in the analysis page, i do not know if there is anything else I can do until the paths i am using are actually created

--- src/analysis/modules/RadarChart/index.js
+++ src/analysis/modules/RadarChart/index.js
@@ -11,25 +11,6 @@ class RadarChart {
   formatData(teams, dataset) {
     console.log(`radar teams recieved: ${teams}`);
     let filteredTeams = teams.filter((team) => team != "|");
-    // previous code start
-    // const values = this.moduleConfig.options.sections.map((section) => {
-    //   const summed = filteredTeams
-    //     .map((team) => {
-    //       let data = getPath(dataset.teams[team], slice.path);
-    //       console.log(`${slice.path}: ${data}`);
-    //       return data;
-    //     })
-    //     .flat()
-    //     .reduce((acc, i) => acc + i, 0);
-    //   if (slice.aggrMethod == "sum") {
-    //     //optionally summed
-    //     return summed;
-    //   } else {
-    //     //default is average
-    //     return (summed / teams.length).toFixed(2);
-    //   }
-    // });
-    //previous code end
 
     const data = [];
     const sectionNames = [];
@@ -51,7 +32,7 @@ class RadarChart {
         type: "scatterpolar",
         r: values,
         theta: sectionNames,
-        fill: "toself",
+        fill: "tonext",
         name: this.moduleConfig.options.lines[i].name,
       };
       data.push(line);
@@ -61,43 +42,24 @@ class RadarChart {
   }
 
   setData(data) {
-    // const layout = {
-    //   margin: {
-    //     pad: 12,
-    //     b: 30,
-    //   },
-    //   title: {
-    //     text: this.moduleConfig.name,
-    //     font: {
-    //       size: 32,
-    //       // color: "#ff6030"
-    //     },
-    //     yanchor: "middle",
-    //   },
-    //   legend: {
-    //     font: {
-    //       size: 20,
-    //     },
-    //     x: 0.85,
-    //   },
-    //   font: {
-    //     family: "Cairo, sans-serif",
-    //   },
-    //   paper_bgcolor: "#FEFEFE",
-    //   plot_bgfcolor: "#FEFEFE",
-    // };
-
-    // const config = {
-    //   responsive: true,
-    //   modeBarButtonsToRemove: ["zoom2d", "pan2d", ""],
-    // };
-
-    //EXAMPLE CODE
     const layout = {
       polar: {
         radialaxis: {
           visible: true,
           range: [0, 40],
+          tickfont: {
+            family: "Cairo, sans-serif",
+            size: 16,
+          },
+          angle:90
+        },
+        angularaxis: {
+          tickfont: {
+            size: 16,
+            family: "Cairo, sans-serif",
+          },
+          direction: "clockwise",
+          rotation: 90,
         },
       },
       margin: {
@@ -108,13 +70,12 @@ class RadarChart {
         text: this.moduleConfig.name,
         font: {
           size: 32,
-          // color: "#ff6030"
         },
         yanchor: "middle",
       },
       legend: {
         font: {
-          size: 20,
+          size: 18,
         },
         x: 0.85,
       },
@@ -124,7 +85,12 @@ class RadarChart {
       paper_bgcolor: "#FEFEFE",
       plot_bgfcolor: "#FEFEFE",
     };
+    
+    const config = {
+      responsive: true
+    };
+    
     Plotly.purge(this.container);
-    Plotly.newPlot(this.container, data, layout);
+    Plotly.newPlot(this.container, data, layout, config);
   }
 }
