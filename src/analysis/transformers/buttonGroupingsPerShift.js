0a5d27f90b30737fbfd33bc23c562d6cf57d12b6
Fatema Merchant <fnmerchant@stu.naperville203.org>
2026-02-10 09:21:19 -0600

    finish first iteration of data transformer

--- src/analysis/transformers/buttonGroupingsPerShift.js
+++ src/analysis/transformers/buttonGroupingsPerShift.js
@@ -4,16 +4,15 @@
  * @param options.ratings {String[]} a list of actionIds that correspond to ratings buttons pressed for a specific action during a certain time interval
  */ 
 _TMP__
-  new DataTransformer("buttonGroupingsPerInterval", (dataset,outputPath,options) => {
+  new DataTransformer("buttonGroupingsPerShift", (dataset,outputPath,options) => {
   for (let tmp of dataset.tmps) {
 
-        let buttonsPressedDuringMatch = {
-            all: [],
-            zone: null,
-            action: null,
-            rating: null,    
+        let output = {
+            all: [], // all will contain all the zones and ratings for this specific match
+            zones: [], // zone represents the zone buttons pressed during a specific match
+            ratings: [], // rating represents the rating buttons pressed during a specific match    
         }
-        
+
         //Options are parameters for this specific data transformer
         //Object.assign copies properties from the source to the option object (and those properties are zones, actions, and ratings, which are all lists of actionIds for the respective buttons pressed during the match sorted by timestamp)
         options = Object.assign({ 
@@ -31,30 +30,29 @@ _TMP__
             let zone = zones.shift(); //remove and get the first zone button pressed
 
             actions = actions.filter(x=>x.ts < zone.ts) //ensure the ratings attributed to a zone occur after the zone
+            
             if (actions.length === 0) break //ensure that the actions button was pressed after the zone button, if there are no actions left to remove, break out of the loop
-            let action = actions.shift();
+                let action = actions.shift();
             
             ratings = ratings.filter(x=>x.ts < action.ts) //ensure the ratings attributed to an action occur after the action
+            
             if(ratings.length === 0) break //ensure that there is a ratings button pressed after the action button, if there are no ratings left to remove, break out of the loop
-            let rating = ratings.shift();
+                let rating = ratings.shift();
 
-            //Based on the zone, action, and rating buttons pressed during a time interval of the match, add that to a list called all, which is a property of the buttonsPressedDuringMatch object. 
-            buttonsPressedDuringMatch.all.push({
-                zone,
-                action,
-                rating,
+            //Based on the zone and rating buttons pressed during a time interval of the match, add that to a list called all, which is a property of the buttonsPressedDuringMatch object. 
+            output.all.push({
+                zones: [zone],
+                ratings: [rating],
             })
         }
-        
-        //Create paths to return in the outputPath object
 
         //For each button grouping in the all property of the buttonsPressedDuringMatch object, create a list of the zones, actions, and ratings for each grouping and set those as properties of the buttonsPressedDuringMatch object.
-        buttonsPressedDuringMatch.zone = buttonsPressedDuringMatch.all.map(x=>x.zone);
-        buttonsPressedDuringMatch.action = buttonsPressedDuringMatch.all.map(x=>x.action);
-        buttonsPressedDuringMatch.rating = buttonsPressedDuringMatch.all.map(x=>x.rating);
+        output.zones = output.all.map(x=>x.zone);
+        output.ratings = output.all.map(x=>x.rating);
 
+        setPath(tmp,outputPath,output);
+        
         }     
-        setPath(tmp,outputPath,buttonsPressedDuringMatch);
         return dataset;
   });
 __/TMP__
