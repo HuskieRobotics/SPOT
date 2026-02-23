795e09e1a1971728137cbed50f78c17e83bc941d
KSM090108 <ksmcc2018@gmail.com>
2026-02-23 09:35:06 -0600

    add initial code for the multiply transformer (have not tested)

---
+++ src/analysis/transformers/multiply.js
@@ -0,0 +1,37 @@
+/**
+ * @type {DataTransformer}
+ * @param options.multiplicands {String[]} array of MatchTeamPerformance outputPaths 
+ */
+__TMP__
+new DataTransformer("multiply", (dataset, outputPath, options) => {
+    for (const tmp of dataset.tmps) {
+      const product = 1;
+        for (const i of options.multiplicands) {
+          product *= getPath(tmp, i, 1);
+        }
+
+        setPath(tmp, outputPath, product)
+    }
+
+    return dataset;
+})
+__/TMP__
+
+/**
+ * @type {DataTransformer}
+ * @param options.multiplicands {String[]} array of Team outputPaths
+ */
+__TEAM__
+new DataTransformer("multiply", (dataset, outputPath, options) => {
+    for (const [teamNumber, team] of Object.entries(dataset.teams)) {
+        const product = 1;
+        for (const i of options.multiplicands) {
+          product *= getPath(team, i, 1);
+        }
+
+        setPath(team, outputPath, product)
+    }
+
+    return dataset;
+})
+__/TEAM__
