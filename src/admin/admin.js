e9b55c4c50cd12cd9fa3539a94954da939404d88
Luke <94772285+ACoderGuy1002@users.noreply.github.com>
2026-03-21 10:16:41 -0500

    Create a button on the admin page to restart the server  (#276)
    This button gets an access code and checks it before restarting to make
    sure that not just anyone can restart SPOT.

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,4 +1,5 @@
 express = require("express");
+const config = require("../../config/config.json");
 
 let router = express.Router();
 
@@ -6,6 +7,16 @@ router.use(express.static(__dirname + "/public"));
 
 router.get("/", (req, res) => {
   res.render(__dirname + "/views/index.ejs");
+});
+
+router.get("/restart", (req, res) => {
+  const key = req.headers.authorization;
+
+  if (key == config.secrets.ACCESS_CODE) {
+    process.exit(0);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 router.use("/api", require("./routes/api.js"));
