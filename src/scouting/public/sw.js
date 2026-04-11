63ebf5680e53c1c0691fdba8650c5028622cff03
Dhriti Kohli <160529366+DhritiKohli@users.noreply.github.com>
2026-04-11 13:42:11 -0500

    Revert "Merge branch 'main' of https://github.com/HuskieRobotics/SPOT"
    This reverts commit 095a4df34af3bffe164154a0698e843acc0812fb, reversing
    changes made to 58394472d0f4d5e2e1ba3e1bd9cca0dd3f939815.

--- src/scouting/public/sw.js
+++ src/scouting/public/sw.js
@@ -45,7 +45,7 @@ const filesToCache = [
   "/analysis/css/internal.css",
   "/analysis/css/style.css",
   "/analysis/js/analysisPipeline.js",
-  // "/analysis/js/autoPick.js",
+  "/analysis/js/autoPick.js",
   "/analysis/js/DataTransformer.js",
   "/analysis/js/elements.js",
   "/analysis/js/script.js",
