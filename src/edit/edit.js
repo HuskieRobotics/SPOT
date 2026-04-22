c53afd1c9a1a2aaf0b020fa50d7f739361a8b3bc
KSM090108 <159940801+KSM090108@users.noreply.github.com>
2026-04-22 10:06:05 -0500

    Add match flagging (#292)
    Add the ability to flag matches in the edit page to be
    reviewed/rescouted later

--- src/edit/edit.js
+++ src/edit/edit.js
@@ -1,5 +1,6 @@
 express = require("express");
 const axios = require("axios");
+const { setPath } = require("../lib/util");
 const config = require("../../config/config.json");
 
 let router = express.Router();
